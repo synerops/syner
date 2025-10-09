dfbdb538e801f5edd81933b3a4b0180b889ffa2a
Ronny Badilla <info@ronnybadilla.com>
2025-10-09 09:28:37 -0600

    feat: cleanup and start the project

--- agent.md
+++
@@ -1,5 +0,0 @@
-# agent
-
-### Created with Turborepo Generators
-
-Read the docs at [turborepo.com](https://turborepo.com/docs/guides/generating-code).
--- packages/agents/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { config as baseConfig } from "@syner/eslint-config/base";
-
-export default [
-  ...baseConfig,
-  {
-    ignores: ["dist/**", "node_modules/**"]
-  }
-];
