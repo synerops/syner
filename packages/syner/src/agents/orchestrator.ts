855db012bb4142f6a99a6e066a0cbd8dfe76b912
Ronny Badilla <info@ronnybadilla.com>
2025-10-13 17:59:37 -0600

    feat: publish package with the necessary files

--- packages/syner/src/agents/orchestrator.ts
+++ packages/syner/src/agents/orchestrator.ts
@@ -1,13 +1,20 @@
-import { Experimental_Agent as Agent } from "ai";
+// import { Experimental_Agent as Agent } from "ai";
 
-import type {
-  OrchestratorTools,
-  OrchestratorOutput,
-  OrchestratorSettings,
-} from "@syner/sdk/agents";
+// import type {
+//   OrchestratorTools,
+//   OrchestratorOutput,
+//   OrchestratorSettings,
+// } from "@syner/sdk/agents";
+import { Orchestrator } from "@syner/sdk/agents";
 
-export class Orchestrator extends Agent<OrchestratorTools, OrchestratorOutput> {
-  constructor(options: OrchestratorSettings) {
-    super(options);
-  }
-}
+// class Orchestrator extends Agent<OrchestratorTools, OrchestratorOutput> {
+//   constructor(options: OrchestratorSettings) {
+//     super(options);
+//   }
+// }
+
+export const orchestrator = new Orchestrator({
+  model: "xai/grok-4-fast-reasoning",
+  system:
+    "You are Syner, an AI agent designed to orchestrate tasks and manage resources within a team.",
+});
