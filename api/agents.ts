8090870804577eeb4e9c88f4c8b539cabbdfdd5d
Ronny Badilla <info@ronnybadilla.com>
2025-10-31 22:24:44 -0600

    feat: vercel deploys the api finally

--- api/agents.ts
+++ api/agents.ts
@@ -1,5 +1,10 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
 const agents = []
 
-export function GET(request: Request) {
-  return Response.json({ agents });
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { name = 'World' } = req.query
+  return res.json({
+    message: `Hello ${name}!`,
+  })
 }
